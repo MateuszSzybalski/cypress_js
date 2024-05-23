749a24b5fe88f2d9feb5bb549a45f4dfb285fe8b
Mateusz Szybalski <mat.szybalski@gmail.com>
2024-05-23 23:02:40 +0200

    update

--- cypress/e2e/tests/test.js
+++ cypress/e2e/tests/test.js
@@ -75,3 +75,15 @@ describe('Sii', () => {
   })
 
 })
+
+describe("Goal.com", () => {
+  it('Visits homepage, opens first article', () => {    
+      cy.visit('https://www.goal.com/en')
+      cy.intercept('*cnsnt.goal.com*', (id) => {
+        id.destroy();
+      })
+      //cy.viewport(1536, 960)
+      cy.title().should('eq', 'Football News, Live Scores, Results & Transfers | Goal.com')
+      cy.get('[data-testid="article-card"]').eq(0).click()
+  })
+})
