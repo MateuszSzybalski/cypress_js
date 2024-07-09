bf1c6c10eee58beb0c74c7e70d50445bcb5027bb
Mateusz Szybalski <mat.szybalski@gmail.com>
2024-07-09 20:27:30 +0200

    add new project

--- cypress/e2e/examples.spec.js
+++ cypress/e2e/examples.spec.js
@@ -1,63 +1,73 @@
 /// <reference types="cypress" />
 
-import { description } from "commander"
-import { multiply } from "lodash"
-import { textChangeRangeIsUnchanged } from "typescript"
+import { ogolne } from "../POM/Ogolne"
 
-describe('Wikipedia.en', () => {
-
-    beforeEach(() => {
-      cy.visit('https://angularjs.realworld.io/')
+describe('Replay', () => {
+  
+beforeEach(() => {
+      cy.visit('https://demo.1crmcloud.com/')
     })
   
-    it('start page', () => {
-      cy.intercept('GET', 'https://api.realworld.io/api/tags').as("requestTags")
-      cy.title().should('eq','Home — Conduit')
-      cy.wait("@requestTags")
-      cy.get("@requestTags").then(res =>  {
-        console.log(res)
-        expect(res.response.statusCode).to.eq(200)
-        expect(res.response.body.tags).length(10)
-        expect(res.response.body.tags).to.contain("eos").and.to.contain("enim")
-        cy.get('[ui-sref="app.login"]').click().then(($singIn) => {
-          let href = $singIn.attr('href')
-          cy.wrap($singIn).click()
-          cy.url().should('contain', href)
+    it('Scenario 1 – Create contact:', () => {
+        cy.fixture("replay").then((loginCredentials) => {
+        cy.get('#login_user').type(loginCredentials.username)
+        cy.get('#login_pass').type(loginCredentials.password)
+        })
+        cy.intercept('POST','**/json.php?action=get_menu_structure').as('requestTag')
+        cy.get('#login_button').click()
+        cy.wait('@requestTag')
+        cy.get('#grouptab-1').should('have.attr', 'title', 'Sales & Marketing')
+        cy.intercept('GET', '**?module=Contacts&action=EditView&return_module=Contacts&return_action=DetailView').as('requestTag2')
+        cy.get('.sidebar-item').contains('Create Contact').click()
+        cy.wait('@requestTag2')
+        cy.wait(2000)
+        cy.fixture("replay").then((userData) => {
+          cy.get('#DetailFormfirst_name-input').type(userData.firstName)
+          cy.get('#DetailFormlast_name-input').type(userData.lastName)
+          cy.get('#DetailFormcategories-input-body').click({force: true})
+          cy.get('#DetailFormcategories-input-search-text').type(userData.role1).type('{enter}')
+          cy.get('#DetailFormcategories-input-search-text').type(userData.role2).type('{enter}')
+          cy.get('#DetailFormbusiness_role-input-label').click({force: true})
+          cy.get('#DetailFormbusiness_role-input-popup').contains(userData.title).click()
+        })         
+        cy.get('#DetailForm_save-label').click()
+        cy.wait(2000)
+        ogolne.saveValue('.listViewNameLink', 0, 'nameAlias')
+        cy.get('.sidebar-item-label').contains('Contacts').click({force:true})
+        cy.get('#filter_text').type('Tester1').type('{enter}')
+        cy.wait(2000)
+        ogolne.compareAliasVal('.listViewNameLink', 'nameAlias', 0)
+        cy.intercept('GET', '**module=Contacts&action=DetailView&record*').as('reqCon')
+        cy.fixture("replay").then((names) => {
+          cy.get('.listViewNameLink').contains(names.firstName + ' ' + names.lastName).click()
+        })
+        cy.get('.listViewNameLink').contains('Tester1' + ' ' + 'Test').click()
+        cy.wait('@reqCon')
+        cy.get('#DetailForm-subpanels').should('be.visible')
+        cy.url().should('contain', 'Contacts&action')
+        cy.get('#DetailForm_personal_data-label').click()
+        cy.fixture('replay').then((nameTitle) => {
+          cy.get('.listViewNameLink').contains(nameTitle.firstName + ' ' + nameTitle.lastName)
+          cy.get('form-value').parent('.column.form-cell.sm-6.cell-business_role.span-1').contains(nameTitle.title)
         })
       })
-    })
-  })
 
-describe('Sii', () => {
-
-  it('Sii test', () => {
-    cy.viewport(1200, 840)
-    cy.visit('https://sii.pl/')
-    cy.get('.cmplz-buttons').contains('Akceptuj wszystkie pliki cookies').click()
-    cy.intercept('POST', 'https://sii.pl/cdn-cgi/rum?').as('requestContact')
-    cy.get('.sii-m-btn-group__item').contains('Wyślij zapytanie').click({force: true})
-    cy.wait('@requestContact')
-    cy.url().should('contain', '/contact-us/')
-    cy.get('#submit_btn').invoke("text").then((buttonText) => {
-      expect(buttonText).is.equal('Wyślij wiadomość')
-      cy.log(buttonText)
-    })
-    cy.get('#submit_btn').invoke("text").then((buttonText) => {
-      cy.get('#find-us').invoke("text").then((button2Text) => {
-        expect(buttonText).is.not.equal(button2Text)
-        cy.log(button2Text)
+      it('Scenario 2 – Run report:', () => {
+        cy.fixture("replay").then((loginCredentials) => {
+        cy.get('#login_user').type(loginCredentials.username)
+        cy.get('#login_pass').type(loginCredentials.password)
+        })
+        cy.intercept('POST','**/json.php?action=get_menu_structure').as('requestTag')
+        cy.get('#login_button').click()
+        cy.wait('@requestTag')
+        cy.get('#grouptab-5').should('have.attr', 'title', 'Reports & Settings').click()
+        cy.get('#filter_text').type('Project Profitability').type('{enter}')
+        cy.intercept('GET', '**/include/javascript/dnduploads.js').as('req')
+        cy.get('.detailLink').contains('Project Profitability').click()
+        cy.wait('@req')
+        cy.intercept('POST', '**/async.php').as('req1')
+        cy.get('[name="FilterForm_applyButton"]').click({force: true})
+        cy.wait('@req1')
+        cy.get('table').find('tr').should('have.length.at.least', 2)
       })
     })
-  })
-
-})
-
-// describe('Goal.com', () => {
-//   it('Visits homepage', () => {    
-//       cy.visit('https://www.goal.com/en')
-//       cy.intercept('*cnsnt.goal.com*', (id) => {
-//         id.destroy();
-//       })
-//       //cy.viewport(1536, 960)
-//   })
-// })
